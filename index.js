56f6f970bee9871b0ccbc62aab1a5a0ec1822fe4
Xinyi Ouyang <oyxyddd@gamil.com>
2023-12-14 14:03:37 -0500

    Add task time

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const addButton = document.querySelector("button");
 console.log(addButton)
 const taskInput = document.getElementById("task-input");
 const taskList = document.getElementById("task-list");
-
+const taskTimeList = document.getElementById("task-time-list"); 
+const timeInput = document.getElementById("taskTime");
 
 //Add event listener 
 addButton.addEventListener("click",addTask);
@@ -11,13 +12,17 @@ addButton.addEventListener("click",addTask);
 //Functions Section 
 function addTask(event){
     const taskContent = taskInput.value.trim();
+    const taskTime = timeInput.value;
 
     if (taskContent == ''){
         taskInput.focus();
         return;
     }else{
         taskList.appendChild(document.createElement("li")).textContent = taskContent;
+        taskTimeList.appendChild(document.createElement("li")).textContent = taskTime;
         taskInput.value = "";
         taskInput.focuse();
     }
+
+
 }
